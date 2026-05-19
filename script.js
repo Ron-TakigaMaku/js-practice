f603916896f2477fd8b11760fc04dadc3c3cf1a4
Yevrey <boyarskiykirill1@gmail.com>
2026-05-19 19:37:56 +0300

    Day 1: Added variables and 10 practice tasks

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
 // // Задача 1
-// Вывести в консоль имя, возраст, статус студента и город проживания
-// const name = 'Ron'
-// let age = 21
-// let isStudent = true
 // const city = 'Kyiv'
-// console.log(name)
-// console.log(age)
-// console.log(isStudent)
 // console.log(city)
 
 // // Задача 2
-// Вывести в консоль  имя и год рождения
 // const name = 'Ron'
 // let age = 22
 // const currentYear = 2026
-// const borthYear = currentYear - age
-// console.log(name, 'was born in', borthYear)
+// const birthYear = currentYear - age
+// console.log(name, 'was born in', birthYear)
 
-// Задача 3
-//Вывести в консоль итоговую цену товара с учетом скидки
-// let price = 100
-// let discount = 20
-// let finalPrice = price - (price * discount) / 100
-// console.log('Final price:', finalPrice)
+// // Задача 3
+// a = 5
+// b = 10
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+
+// // Задача 4
+// const profession = 'Frontend Developer'
+// console.log(profession.length)
+
+// Задача 5
+// const name = 'Ron'
+// let age = 22
+// console.log(`My name is ${name} and my age is ${age}`)
+
+// Задача 6
+// Используй template literal (обратные кавычки  ), чтобы вывести ту же фразу.
+// я не знаю как это делать
+
+// Задача 7
+// const price = 1000
+// const discount = 15
+// const discountedPrice = price - (price * discount) / 100
+// console.log(`Discounted price: ${discountedPrice}`)
+
+// Задача 8
+// const strNumber = '25'
+// const number = parseInt(strNumber)
+// const results = number + 10
+// console.log(results)
+
+// Задача 9
+// я не знаю как это делать
+
+// Задача 10
+// let age = 21
+// if (age >= 18) {
+// 	console.log('You are an adult')
+// } else {
+// 	console.log('You are under 18')
+// }
