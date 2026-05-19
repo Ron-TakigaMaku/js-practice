c0a29eb05ffb490db9ab2b3cca5d858823d276a3
Yevrey <boyarskiykirill1@gmail.com>
2026-05-19 17:27:54 +0300

    add day 1 js practice

---
+++ script.js
@@ -0,0 +1,25 @@
+// // Задача 1
+// Вывести в консоль имя, возраст, статус студента и город проживания
+// const name = 'Ron'
+// let age = 21
+// let isStudent = true
+// const city = 'Kyiv'
+// console.log(name)
+// console.log(age)
+// console.log(isStudent)
+// console.log(city)
+
+// // Задача 2
+// Вывести в консоль  имя и год рождения
+// const name = 'Ron'
+// let age = 22
+// const currentYear = 2026
+// const borthYear = currentYear - age
+// console.log(name, 'was born in', borthYear)
+
+// Задача 3
+//Вывести в консоль итоговую цену товара с учетом скидки
+// let price = 100
+// let discount = 20
+// let finalPrice = price - (price * discount) / 100
+// console.log('Final price:', finalPrice)
